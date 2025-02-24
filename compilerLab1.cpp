0fff0ed6b8346f16e0f08d2dba06d63cb2a40d84
Fahima Abida Chowdhury <165580535+abida3564@users.noreply.github.com>
2025-02-25 01:19:57 +0600

    compilerLab1.cpp

--- compilerLab1.cpp
+++ compilerLab1.cpp
@@ -8,7 +8,7 @@ int main(){
       cout<<"Yes"<<endl;
     }
     else if((s[0] =='/') && (s[1]=='*') && (s[s.size()-2]=='*') && (s[s.size()-1]=='/'))
-      cout<<"YES"<<endl;
+      cout<<"Commenting"<<endl;
 
     else cout<<"No"<<endl;
 }
